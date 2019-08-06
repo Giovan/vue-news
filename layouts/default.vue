8c431ed3960b0de5bef09ecff82ba8678a2aacaf
Giovanni <giovannidelgado@iMac-de-Giovanni.local>
2019-08-06 12:00:53 -0500

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <section class="hero has-text-centered is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">PWA News</h1>
+          <h2 class="subtitle subtitle-edit">All the headlines making the wavy!</h2>
+        </div>
+      </div>
+    </section>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+  html {
+    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  }
+
+  .hero-body {
+    padding: 0!important
+  }
+
+  .container {
+    min-height: 5vh!important
+  }
+
+  .subtitle-edit {
+    margin-top: 0rem!important;
+    padding-bottom: 0px!important
+  }
+</style>
